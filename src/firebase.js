fe057792f27f1eb1edecb6b66372e2b791cd58bd
DhairyaBahl <dhairyabahl5@gmail.com>
2021-02-09 16:13:42 +0530

    adding correct firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,17 @@
-import firebase from "firebase"
+import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCOTv_d5YPVLAi3Ixnl5GNWgEukRg-0g5A",
-  authDomain: "i-clone0.firebaseapp.com",
-  databaseURL: "https://i-clone0-default-rtdb.firebaseio.com",
-  projectId: "i-clone0",
-  storageBucket: "i-clone0.appspot.com",
-  messagingSenderId: "303716436538",
-  appId: "1:303716436538:web:155ce3f44c1157603b4303",
-  measurementId: "G-80QBKSD154"
-};
+const firebaseConfig={
+  apiKey:"AIzaSyDBvyyqdV630DffF6sVYP6v3TZWZjHxamg",
+  authDomain:"mui-message.firebaseapp.com",
+  databaseURL:"https://mui-message-default-rtdb.firebaseio.com",
+  projectId:"mui-message",
+  storageBucket:"mui-message.appspot.com",
+  messagingSenderId:"506461386312",
+  appId:"1:506461386312:web:45f666e6778fed26d9ca7d",
+  measurementId:"G-HVDGKL164W"
+}
 
 const firebaseApp=firebase.initializeApp(firebaseConfig);
 const db=firebaseApp.firestore();
-const storage=firebase.storage();
-const auth=firebase.auth();
 
-export {db,auth,storage}
+export default db;
