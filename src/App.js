31ce6cba1fde03bd3e60a8252ad9889fe30931f4
Pavel Darii <paveldarii@yahoo.com>
2021-02-28 11:27:41 -0500

    added functionality that when we push to send message, the window should scroll to the bottom

--- src/App.js
+++ src/App.js
@@ -1,88 +1,146 @@
-import './App.css';
-import {useState,useEffect,useRef} from 'react';
-import {Button, Input,FormControl} from "@material-ui/core";
-import Brightness4Icon from "@material-ui/icons/Brightness4"
-import SendIcon from '@material-ui/icons/Send'
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import { Button, Input, FormControl } from "@material-ui/core";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import SendIcon from "@material-ui/icons/Send";
 //import {IconButton} from '@material-ui/core'
-import logo from './logo.png';
-import Messages from './Messages.js'
-import db from "./firebase.js"
+import logo from "./logo.png";
+import Messages from "./Messages.js";
+import db from "./firebase.js";
 import firebase from "firebase";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+  const [dark, setDark] = useState(false);
+  const scrollToBottom = () => {
+    endRef.current.scrollIntoView({ behaviour: "smooth" });
+  };
 
-  const [input,setInput]=useState("");
-  const [messages,setMessages]=useState([]);
-  const [username,setUsername]=useState("");
-  const [dark,setDark]=useState(false);
-  const scrollToBottom=()=>{
-    endRef.current.scrollIntoView({behaviour:"smooth"})
-  }
+  useEffect(scrollToBottom, []);
 
-  useEffect(scrollToBottom,[]);
-
-  useEffect(()=>{
+  useEffect(() => {
     setUsername(prompt("Kindly Enter Your Name"));
-  },[])
+  }, []);
 
-  useEffect(()=>{db.collection('messages').orderBy("timestamp","asc").onSnapshot(snapshot=>setMessages(snapshot.docs.map(doc=>doc.data())))},[])
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, []);
 
-  const endRef=useRef(null);
+  const endRef = useRef(null);
 
-  const newMessage=(event)=>{
+  const newMessage = (event) => {
     event.preventDefault();
     //setMessages([...messages,{message:input,username:username}]);
-    if(input!=="")
-    {
-      db.collection('messages').add({username:username,message:input,timestamp:firebase.firestore.FieldValue.serverTimestamp()})
+    if (input !== "") {
+      db.collection("messages").add({
+        username: username,
+        message: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
     }
-    setInput("");
-  }
 
-  const theme=(event)=>{
-    if(dark===false)
-    {
-      document.body.classList.add('dark-bg');
+    setInput("");
+
+    //scrolls to the bottom of the page
+    document
+      .getElementById("root")
+      .scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+  };
+
+  const theme = (event) => {
+    if (dark === false) {
+      document.body.classList.add("dark-bg");
       setDark(true);
-    }
-    else
-    {
-      document.body.classList.remove('dark-bg');
+    } else {
+      document.body.classList.remove("dark-bg");
       setDark(false);
     }
-  }
+  };
 
   let but;
-  if(dark)
-  {
-    but=<input className={`input ${dark?"dark_input":""}`} placeholder="Write Your Message" value={input} onChange={event=>setInput(event.target.value)} />
-  }
-  else
-  {
-    but=<Input className={`input ${dark?"dark_input":""}`} placeholder="Write Your Message" value={input} onChange={event=>setInput(event.target.value)} />
+  if (dark) {
+    but = (
+      <input
+        className={`input ${dark ? "dark_input" : ""}`}
+        placeholder="Write Your Message"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+    );
+  } else {
+    but = (
+      <Input
+        className={`input ${dark ? "dark_input" : ""}`}
+        placeholder="Write Your Message"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+    );
   }
 
   return (
     <div className="App">
-      <nav className={`NavBar ${dark?"BlackNavBar":""}`} >
-        <Button variant="contained" className="dark" onClick={theme} ><span style={{fontWeight:"bold",fontSize:"20px"}} ></span><Brightness4Icon /></Button>
-        <h1 className="messenger" ><span className={`${dark?"blackName":""} `} style={{color:"orange"}}>Mess</span><span className={`${dark?"blackName":""} `}  style={{color:"deeppink"}} >enger</span></h1>
+      <nav className={`NavBar ${dark ? "BlackNavBar" : ""}`}>
+        <Button variant="contained" className="dark" onClick={theme}>
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}></span>
+          <Brightness4Icon />
+        </Button>
+        <h1 className="messenger">
+          <span
+            className={`${dark ? "blackName" : ""} `}
+            style={{ color: "orange" }}
+          >
+            Mess
+          </span>
+          <span
+            className={`${dark ? "blackName" : ""} `}
+            style={{ color: "deeppink" }}
+          >
+            enger
+          </span>
+        </h1>
         <img className="Logo" src={logo} alt="messenger-logo" />
       </nav>
-      <div >
-        <br/><br/><br/><br/><br/>
-        {
-          messages.map(message=><Messages messages={message} username={username} dark={dark} />)
-        }
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {messages.map((message) => (
+          <Messages messages={message} username={username} dark={dark} />
+        ))}
         <div ref={endRef} />
-        <br/><br/><br/><br/><br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
-      <footer className={`${dark?"footer_dark":""}`} >
+      <footer className={`${dark ? "footer_dark" : ""}`}>
         <form>
-          <FormControl>
-            {but}
-          </FormControl>
-          <Button className="iconButton" onClick={newMessage} type="submit" variant="contained" color="primary" > <SendIcon /></Button>
+          <FormControl>{but}</FormControl>
+          <Button
+            className="iconButton"
+            onClick={newMessage}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            {" "}
+            <SendIcon />
+          </Button>
         </form>
       </footer>
     </div>
