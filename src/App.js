ca8dcacf02d60b88f80b0762ab8118b2325c5173
unknown <dhairyabahl5@gmail.com>
2021-03-18 15:45:39 +0530

    adding some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import { Button } from "@material-ui/core";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
-// import SendIcon from "@material-ui/icons/Send";
 import logo from "./logo.png";
 import Messages from "./Messages.js";
 import WelcomeDialogBox from './WelcomeDialogBox'
@@ -13,7 +12,6 @@ function App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
-  const [dark, setDark] = useState(false);  
   const [openWelcomeDialogBox,setOpenWelcomeDialogBox]=useState(false)
   const [dark, setDark] = useState(false);
   const messagesEndRef = useRef(null);
@@ -40,7 +38,6 @@ function App() {
 
   const newMessage = (event) => {
     event.preventDefault();
-    //setMessages([...messages,{message:input,username:username}]);
     if (input !== "") {
       db.collection("messages").add({
         username: username,
@@ -61,31 +58,11 @@ function App() {
     }
   };
 
-  // let but;
-  // but = (
-  //   <input
-  //     className={`input ${dark ? "dark_input" : "light_input"}`}
-  //     placeholder="Write Your Message"
-  //     value={input}
-  //     onChange={(event) => setInput(event.target.value)}
-  //   />
-  // );
-  // if(dark)
-  // {
-  //   but=<input className={`input ${dark?"dark_input":""}`} placeholder="Write Your Message" value={input} onChange={event=>setInput(event.target.value)} />
-  // }
-  // else
-  // {
-  //   but=<Input className={`input ${dark?"dark_input":""}`} placeholder="Write Your Message" value={input} onChange={event=>setInput(event.target.value)} />
-  // }
-
   return (
     <div className="App">
       <nav className={`NavBar ${dark ? "BlackNavBar" : ""}`}>
         <div className="flex1">
           <img className="Logo"  aspect-ratio="1/1" height="auto" width="82px"  src={logo} alt="messenger-logo" />
-          {/*<h1 className="messenger" ><span className={`${dark?"blackName":""} `}
-          style={{color:"orange"}}>Mess</span><span className={`${dark?"blackName":""} `}  style={{color:"deeppink"}} >enger</span></h1>*/}
           <h1 className={`messenger ${dark ? "blackName" : ""}`}>Messenger</h1>
         </div>
         <div className="flex2">
@@ -100,11 +77,7 @@ function App() {
         </div>
       </nav>
       <div className="scroll">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <br /><br /><br /><br /><br />
         {messages.map((message) => (
           <Messages
             messages={message}
@@ -114,11 +87,7 @@ function App() {
           />
         ))}
         <div />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <br /><br /><br /><br /><br />
       </div>
       <div ref={messagesEndRef} />
       <div className="div__footer">
@@ -147,8 +116,7 @@ function App() {
   );
 }
 
-{/* */}
-{/* <form>
+<form>
           <FormControl>{but}</FormControl>
           <Button
             className="iconButton"
@@ -159,11 +127,11 @@ function App() {
             {" "}
             <SendIcon />
           </Button>
-        </form> */
-        </form>
-      </footer>
+</form>
+{
       <WelcomeDialogBox open={openWelcomeDialogBox} close={()=>setOpenWelcomeDialogBox(false)} setUsername={setUsername}/>
-    </div>
+}
+      </div>
   );
 }
 
