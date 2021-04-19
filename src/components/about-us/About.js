0bf53fbfcabb6391a453dc796b63bba95d9d61e4
Shivangi-ch <shivangi.ch.936@gmail.com>
2021-04-19 13:14:43 +0530

    dark theme for about us

--- src/components/about-us/About.js
+++ src/components/about-us/About.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import logo from "../../logo.png";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import "./Abouts.css";
+
 
 const About = () => {
     return (
