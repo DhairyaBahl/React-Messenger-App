fac624b59ab05986cbc61f56e82d913a3e37c385
garvitgupta13 <garvitgupta2001@gmail.com>
2021-05-13 16:25:43 +0530

    Clean the codebase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useState, useEffect, useRef } from "react";
 import { Button } from "@material-ui/core";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import logo from "./logo.png";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Messagesentaudio from "./sound/MessageSound.mp3";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Messages from "./components/messages/Messages.js";
 import WelcomeDialogBox from "./WelcomeDialogBox";
 import db from "./firebase.js";
@@ -14,15 +14,17 @@ import firebase from "firebase";
 import About from "./components/about-us/About";
 import Footer from "./components/footer/footer";
 import ContactUs from "./components/contactForm/contactForm.js";
-import 'emoji-mart/css/emoji-mart.css';
-import { Picker } from 'emoji-mart';
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import { purple } from "@material-ui/core/colors";
 import Landing from "./components/Landingpage/LandingPage";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
 
 function App() {
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
@@ -33,41 +35,39 @@ function App() {
   const inputElement = useRef(null);
   const [click, setClick] = useState(false);
   const [showEmojis, setshowEmojis] = useState(false);
-  const { finalTranscript,resetTranscript } = useSpeechRecognition();
+  const { finalTranscript, resetTranscript } = useSpeechRecognition();
 
-
+  //Open the dialog box for getting th user name
   useEffect(() => {
     setOpenWelcomeDialogBox(true);
   }, []);
 
+  //Show the loading symbol until all messages are populated from firebase
   useEffect(() => {
-    setLoading(true)
-    console.log("setting true",loading)
+    setLoading(true);
+    console.log("setting true", loading);
     db.collection("messages")
       .orderBy("timestamp", "asc")
-      .onSnapshot((snapshot) =>{
+      .onSnapshot((snapshot) => {
         setMessages(snapshot.docs.map((doc) => doc.data()));
-        setLoading(false)
+        setLoading(false);
       });
   }, []);
 
+  //As the page reloads move to bottom
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
-  
+
   const handleClick = () => setClick(!click);
 
+  //Function to scroll to the bottom of all messages
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "auto" });
   };
 
-
-
-  
-
+  //Funnction to create the new message and store it in forebase
   const newMessage = (event) => {
-    //event.preventDefault();
-    //setMessages([...messages,{message:input,username:username}]);
     if (input.trim() !== "") {
       db.collection("messages").add({
         username: username,
@@ -78,17 +78,19 @@ function App() {
       new Audio(Messagesentaudio).play();
     }
     setInput("");
-    
   };
 
+  //Function to send message if the enter key is pressed
   const handleKeypress = (event) => {
-    console.log("yes")
+    console.log("yes");
     //it triggers by pressing the enter key
-  if (event.key === 'Enter') {
-    console.log("13");
-    newMessage()
-  }
-};
+    if (event.key === "Enter") {
+      console.log("13");
+      newMessage();
+    }
+  };
+
+  //Function for changing the theme
   const theme = (event) => {
     if (dark === false) {
       document.body.classList.add("dark-bg");
@@ -98,211 +100,216 @@ function App() {
       setDark(false);
     }
   };
-  
+
+  //Function to add emoji in text
   const addEmoji = (e) => {
     let emoji = e.native;
     let cursorPositionStart = inputElement.current.selectionStart;
-    let newinput= input.slice(0, cursorPositionStart) + emoji + input.slice(cursorPositionStart);
+    let newinput =
+      input.slice(0, cursorPositionStart) +
+      emoji +
+      input.slice(cursorPositionStart);
     setInput(newinput);
-    inputElement.current.focus()
+    inputElement.current.focus();
   };
   const emojiToggle = (e) => {
     console.log("in emojiToggle");
-    if(showEmojis === true)
-    {
+    if (showEmojis === true) {
       setshowEmojis(false);
       console.log("picker not visible");
-    }
-    else
-    {
+    } else {
       setshowEmojis(true);
       console.log("picker visible");
     }
   };
- 
 
   useEffect(() => {
-    if(finalTranscript !== "")
-    {
+    if (finalTranscript !== "") {
       setInput(finalTranscript);
       resetTranscript();
     }
   });
-   const Speechtoinput = (e) => {
+  const Speechtoinput = (e) => {
     SpeechRecognition.startListening();
   };
- 
 
   return (
-
     <Router>
+      {/*================ NavBar. Common across all routes ======================*/}
 
-    {/*================ NavBar. Common across all routes ======================*/}
+      <nav className={`${dark ? "nav_dark" : "navbar"}`}>
+        <div className="nav-container">
+          <a href="/landing">
+            <img
+              className="Logo"
+              aspect-ratio="1/1"
+              height="auto"
+              width="50px"
+              src={logo}
+              alt="messenger-logo"
+            />
+          </a>
+          <h1 style={{ fontSize: "25px" }} className={`messenger`}>
+            Messenger
+          </h1>
+          <a href="/" className="nav-logo"></a>
 
-    <nav className={`${dark ? "nav_dark" : "navbar"}`}>
-    <div className="nav-container">
-      <a href="/landing">
-        <img
-            className="Logo"
-            aspect-ratio="1/1"
-            height="auto"
-            width="50px"
-            src={logo}
-            alt="messenger-logo"
-          />
-        </a>
-        <h1 style={{fontSize: "25px"}} className={`messenger`}>Messenger</h1> 
-      <a href="/" className="nav-logo">
-      
-      </a>
-
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item">
-          <Link
-            to="/"
-            activeClassName="active"
-            className="nav-links"
-            onClick={handleClick}
-          >
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/about"
-            activeClassName="active"
-            className="nav-links"
-            onClick={handleClick}
-          >
-            About Us
-          </Link>
-        </li>
-        <li className="nav-item toggle-nav" style={{border: "none"}}>
-            <Button
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item toggle-nav" style={{ border: "none" }}>
+              <Button
                 title="toggle Dark Mode"
                 className="dark toggle-button"
                 onClick={theme}
-            >
-                <Brightness4Icon className="darkthemeicon"/>
-            </Button>
-        </li>
-      </ul>
-      <div className="nav-icon" onClick={handleClick}>
-        <i><MenuIcon style={{fontSize: "30px", marginTop:"3px"}}/></i>
-      </div>
-    </div>
-  </nav>
-  
-  {/*========================== End of NavBar ============================*/}
-           
-     <Switch>
-
-      {/*========================== about us ============================*/}
-      
-      <Route path="/about">
-      <About />
-      <ContactUs apptheme={dark}/>
-      <Footer />
-      </Route>
-      {/*========================== landing page ============================*/}
-
-      <Route path="/landing">
-      <Landing/>
-      <Footer />
-      </Route>
-
-    {/*========================== home page ============================*/}
-
-      <Route path="/">
-        
-        <div className="App">
-        {
-            loading?<CircularProgress className="loading"/>:
-            <>
-            <div className="scroll">
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                {messages.map((message) => (
-                <Messages
-                    messages={message}
-                    username={username}
-                    uid={uid}
-                    dark={dark}
-                    key={genKey()}
-                />
-                ))}
-                <div />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-            </div>
-            <div ref={messagesEndRef} />
-            <div className="div__footer">
-                <footer className={`${dark ? "footer_dark" : ""}`}>
-                <div className="content__footer">
-                    <div className="sendNewMessage">
-                    <button className={`addfiles ${dark ? "darkButton" : ""}`}>
-                        <i className="fa fa-plus"></i>
-                    </button>
-                    <button className="EmojiToggle"><InsertEmoticonIcon onClick={emojiToggle}/></button>
-                 { showEmojis && <span className="EmojiPicker"><Picker onSelect={addEmoji}/></span> }
-                    <input
-                        ref={inputElement}
-                        className={`input ${dark ? "dark_input" : "light_input"}`}
-                        type="text"
-                        placeholder="Type a message"
-                        onChange={(event) => setInput(event.target.value)}
-                        onKeyPress={handleKeypress}
-                        value={input}
-                    />
-                    <div className="speak">
-                       <button onClick={Speechtoinput}><i className="fa fa-microphone" ></i></button>
-                    </div>
-
-                    <button
-                        className={`btnsend ${dark ? "darkButtonSend" : ""}`}
-                        id="sendMsgBtn"
-                        type="submit"
-                        variant="contained"
-                        crossOrigin="anonymous"
-                        onClick={newMessage}
-                    >
-                        <i className="fa fa-paper-plane"></i>
-                    </button>
-                    </div>
-                </div>
-                </footer>
-                <WelcomeDialogBox
-                open={openWelcomeDialogBox}
-                close={() => setOpenWelcomeDialogBox(false)}
-                setUsername={setUsername}
-                setUid={setUid}
-                />
-            </div>
-            </>
-        }
+              >
+                <Brightness4Icon className="darkthemeicon" />
+              </Button>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i>
+              <MenuIcon style={{ fontSize: "30px", marginTop: "3px" }} />
+            </i>
+          </div>
         </div>
+      </nav>
 
-      </Route>
+      {/*========================== End of NavBar ============================*/}
 
+      <Switch>
+        {/*========================== about us ============================*/}
+
+        <Route path="/about">
+          <About />
+          <ContactUs apptheme={dark} />
+          <Footer />
+        </Route>
+        {/*========================== landing page ============================*/}
+
+        <Route path="/landing">
+          <Landing />
+          <Footer />
+        </Route>
+
+        {/*========================== home page ============================*/}
+
+        <Route path="/">
+          <div className="App">
+            {loading ? (
+              <CircularProgress className="loading" />
+            ) : (
+              <>
+                <div className="scroll">
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  {messages.map((message) => (
+                    <Messages
+                      messages={message}
+                      username={username}
+                      uid={uid}
+                      dark={dark}
+                      key={genKey()}
+                    />
+                  ))}
+                  <div />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                </div>
+                <div ref={messagesEndRef} />
+                <div className="div__footer">
+                  <footer className={`${dark ? "footer_dark" : ""}`}>
+                    <div className="content__footer">
+                      <div className="sendNewMessage">
+                        <button
+                          className={`addfiles ${dark ? "darkButton" : ""}`}
+                        >
+                          <i className="fa fa-plus"></i>
+                        </button>
+                        <button className="EmojiToggle">
+                          <InsertEmoticonIcon onClick={emojiToggle} />
+                        </button>
+                        {showEmojis && (
+                          <span className="EmojiPicker">
+                            <Picker onSelect={addEmoji} />
+                          </span>
+                        )}
+                        <input
+                          ref={inputElement}
+                          className={`input ${
+                            dark ? "dark_input" : "light_input"
+                          }`}
+                          type="text"
+                          placeholder="Type a message"
+                          onChange={(event) => setInput(event.target.value)}
+                          onKeyPress={handleKeypress}
+                          value={input}
+                        />
+                        <div className="speak">
+                          <button onClick={Speechtoinput}>
+                            <i className="fa fa-microphone"></i>
+                          </button>
+                        </div>
+
+                        <button
+                          className={`btnsend ${dark ? "darkButtonSend" : ""}`}
+                          id="sendMsgBtn"
+                          type="submit"
+                          variant="contained"
+                          crossOrigin="anonymous"
+                          onClick={newMessage}
+                        >
+                          <i className="fa fa-paper-plane"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </footer>
+                  <WelcomeDialogBox
+                    open={openWelcomeDialogBox}
+                    close={() => setOpenWelcomeDialogBox(false)}
+                    setUsername={setUsername}
+                    setUid={setUid}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        </Route>
       </Switch>
-</Router>
-
-
-);
+    </Router>
+  );
 }
 
 // keys generator:- every new call to this function will give numbs like 0,1,2,3....
 const genKey = (function () {
-var keyCode = 0;
-return function incKey() {
-return keyCode++;
-};
+  var keyCode = 0;
+  return function incKey() {
+    return keyCode++;
+  };
 })();
 
 export default App;
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
-import './Messages.css';
+import React, { useState } from "react";
+import "./Messages.css";
 
+// Function for Read More feature (If the text is more than 150 characters than hide the rest and add a read more link)
 const ReadMore = ({ children, length = 180 }) => {
   const text = children;
   const [isReadMore, setIsReadMore] = useState(true);
   const toggleReadMore = () => {
     setIsReadMore(!isReadMore);
   };
-  if(text === null)
-  {
+  if (text === null) {
     return <p>{text}</p>;
   }
   if (text.length < length) {
@@ -24,21 +24,34 @@ const ReadMore = ({ children, length = 180 }) => {
   );
 };
 
-function Messages(props){
-  const isUser = props.messages.uid===props.uid;
+function Messages(props) {
+  const isUser = props.messages.uid === props.uid;
   return (
-    <div className="message_div" >
-      <h5 className={`sender ${props.dark?"senderDark":""}`} >{`${isUser?"":props.messages.username===null?"Unknown user":props.messages.username}`}</h5>
-      <div className={`msgs ${isUser?"user":"guest"} ${props.dark?"msgsDark":""} `} >
-      <ReadMore>{props.messages.message}</ReadMore>
-        <p className={`time-stamp ${props.dark?"time-stampDark":""}`}>{giveTimePassed(props.messages.timestamp)}</p>
+    <div className="message_div">
+      <h5 className={`sender ${props.dark ? "senderDark" : ""}`}>{`${
+        isUser
+          ? ""
+          : props.messages.username === null
+          ? "Unknown user"
+          : props.messages.username
+      }`}</h5>
+      <div
+        className={`msgs ${isUser ? "user" : "guest"} ${
+          props.dark ? "msgsDark" : ""
+        } `}
+      >
+        <ReadMore>{props.messages.message}</ReadMore>
+        <p className={`time-stamp ${props.dark ? "time-stampDark" : ""}`}>
+          {giveTimePassed(props.messages.timestamp)}
+        </p>
       </div>
     </div>
   );
 }
 
-function giveTimePassed(timestamp){
-  if(!timestamp){
+//Function to get the time passed after the message is sent
+function giveTimePassed(timestamp) {
+  if (!timestamp) {
     return "just now";
   }
   // get date, month and year from timestamp
@@ -47,53 +60,59 @@ function giveTimePassed(timestamp){
   var sentYear = timestamp.toDate().getFullYear();
   var sentHours = timestamp.toDate().getHours();
   var sentMinutes = timestamp.toDate().getMinutes();
-  // var sentSeconds = timestamp.toDate().getSeconds();
-  
+
   // time difference in millisec
   var timeDiff = new Date() - timestamp.toDate();
-   // seconds
+  // seconds
   timeDiff /= 1000;
-  timeDiff = Math.floor(timeDiff)
-  
-  if(timeDiff < 5){
+  timeDiff = Math.floor(timeDiff);
+
+  if (timeDiff < 5) {
     return "just now";
   }
-  if(timeDiff < 60){
-    return `${timeDiff} second${timeDiff===1?"":"s"} ago`;
+  if (timeDiff < 60) {
+    return `${timeDiff} second${timeDiff === 1 ? "" : "s"} ago`;
   }
-   // minutes
+  // minutes
   timeDiff /= 60;
-  timeDiff = Math.floor(timeDiff)
-  if(timeDiff < 60){
-    return `${timeDiff} minute${timeDiff===1?"":"s"} ago`;
+  timeDiff = Math.floor(timeDiff);
+  if (timeDiff < 60) {
+    return `${timeDiff} minute${timeDiff === 1 ? "" : "s"} ago`;
   }
-  
+
   // hours
   timeDiff /= 60;
-  timeDiff = Math.floor(timeDiff)
-  if(timeDiff < 60){
-    return `${timeDiff} hour${timeDiff===1?"":"s"} ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
+  timeDiff = Math.floor(timeDiff);
+  if (timeDiff < 60) {
+    return `${timeDiff} hour${
+      timeDiff === 1 ? "" : "s"
+    } ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
   }
-  
+
   // days
   timeDiff /= 24;
-  timeDiff = Math.floor(timeDiff)
-  if(timeDiff < 30){
-    return `${timeDiff} day${timeDiff===1?"":"s"} ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
+  timeDiff = Math.floor(timeDiff);
+  if (timeDiff < 30) {
+    return `${timeDiff} day${
+      timeDiff === 1 ? "" : "s"
+    } ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
   }
-  
+
   // months
   timeDiff /= 30;
-  timeDiff = Math.floor(timeDiff)
-  if(timeDiff < 12){
-    return `${timeDiff} month${timeDiff===1?"":"s"} ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
+  timeDiff = Math.floor(timeDiff);
+  if (timeDiff < 12) {
+    return `${timeDiff} month${
+      timeDiff === 1 ? "" : "s"
+    } ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
   }
-  
+
   // years
   timeDiff /= 12;
-  timeDiff = Math.floor(timeDiff)
-  return `${timeDiff} year${timeDiff===1?"":"s"} ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
- }
- 
+  timeDiff = Math.floor(timeDiff);
+  return `${timeDiff} year${
+    timeDiff === 1 ? "" : "s"
+  } ago | ${sentDate}/${sentMonth}/${sentYear} | ${sentHours}:${sentMinutes}`;
+}
 
 export default Messages;
