64adf4dc242bc1eb4f2fe4328459cb2dc4ba073b
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-29 17:41:36 +0530

    preloader made

---
+++ src/components/preloader/preloader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles.css";
+
+function Preloader() {
+    return (
+        <div className='preloader'>
+            <div className='updown'>
+                <h1>
+                    Messanger<span> .</span>
+                    <span> .</span>
+                    <span> .</span>
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Preloader;
