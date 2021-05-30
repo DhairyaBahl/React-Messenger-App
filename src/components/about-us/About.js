e2bc81c58c937325b1d0d00a129ce18a4e087c52
tanishq <tanishqarya24@gmail.com>
2021-05-30 15:56:08 +0530

    fixed about for mobile

--- src/components/about-us/About.js
+++ src/components/about-us/About.js
@@ -8,7 +8,7 @@ import { ReactComponent as Logo2 } from "../../about-2.svg";
 const About = (props) => {
     return (
         <div>
-            <Grid container spacing={2} className={`${props.apptheme5 ? "main" : "main_light"}`} id="about">
+            <Grid container className={`${props.apptheme5 ? "about_main" : "about_main_light"}`} id="about">
                 <Grid item xs={12} sm={12} md={12} lg={12} style={{ marginBottom: "-20px" }}>
                     <h1 id="brand">React Messenger App</h1>
                 </Grid>
