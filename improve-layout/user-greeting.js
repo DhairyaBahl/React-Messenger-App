fc4c98b4acf36d432a50af25bd95ec491af634d4
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-31 19:47:28 +0530

    added user greeting feature

---
+++ improve-layout/user-greeting.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+// components
+import Logout from './Logout'
+
+const UserGreeting = (props) => {
+
+  // Initialize initial state and its modifier function
+  const [logoutData, setLogoutData] = useState({ showLogout: false })
+
+  const showHideLogout = () => {
+    setLogoutData({ ...logoutData, showLogout: !logoutData.showLogout })
+  }
+
+  const { username } = props
+  const { showLogout } = logoutData
+
+  return (
+    <div className="user-info" onClick={showHideLogout}>
+      <span className="user-greeting">Hi {username}</span>
+      { (showLogout == true) && <Logout />}
+    </div>
+  );
+};
+
+export default UserGreeting;
